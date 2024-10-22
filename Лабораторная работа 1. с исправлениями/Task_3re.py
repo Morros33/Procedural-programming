2154a80872eaab39a1a82c570c900084961de9a9
Morros33 <nikiozoz@outlook.com>
2024-10-22 19:54:40 +0300

    Add files via upload

---
+++ Лабораторная работа 1. с исправлениями/Task_3re.py
@@ -0,0 +1,11 @@
+# TODO Найдите количество книг, которое можно разместить на дискете
+
+
+sym_s, lines, pages = 24, 50, 100
+byte, kbyte = 4, 1024
+cap = 1.44
+book = byte * sym_s * lines * pages
+disc = cap * kbyte**2
+shelf = round(disc // book)
+
+print("Количество книг, помещающихся на дискету:", shelf)
